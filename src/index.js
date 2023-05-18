d3308fcd5d0c663d183b07da64fdaa8d734667f8
Niky-Niky <105742815+Niky-Niky@users.noreply.github.com>
2023-05-18 18:12:49 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,38 +1,51 @@
-// import DATABASE_POSTS from "./db-var";
-// import DATABASE_COMMENTS from "./db-var";
-// import DATABASE_PROFILE from "./db-var";
-// import DATABASE_PRODUCTS from "./db-var";
-//1 зробити фетч за продуктами
-//2 створити розмітку одного елементу списку
-//3 за допомогою map перебераємо масив товарів
-//4 додаємо в розмітку елементи списку
+const BASE_URL =  ' http://localhost:3000'
+ const ulCardEl = document.querySelector('#products-list')
 
-const DATABASE_POSTS = 'http://localhost:3000/posts'
-const DATABASE_COMMENTS = 'http://localhost:3000/comments'
-const DATABASE_PROFILE = 'http://localhost:3000/profile'
-const DATABASE_PRODUCTS = 'http://localhost:3000/products'
 
-const myList = document.querySelector('#products');
-async function getProduct (){
-const res = await fetch(DATABASE_PRODUCTS);
-console.log(res);
-const products = await res.json()
-console.log(products)
-return products
+
+ async  function getProdutcts() {
+    try{
+        const  res = await fetch(`${BASE_URL}/products`)
+        const products =  await res.json()
+        console.log(products);
+        return products
+    } catch(error){
+console.log(error);
+    }
+    
 }
-// console.log(products);
-console.log(getProduct());
-// console.log(products);
-function makeMarkup (products){
-    return products.id?.map(({image, name}) => {
-        return `<li> 
+
+
+function makeMarkup (products) {
+  return  products.map(({image, name})=>{
+        return `    <li>
         <img src="${image}" alt="${name}">
         <p>${name}</p>
-        <button type="button">Add to card</button>
-        </li>`
+        <button type="button ">Додати в кошик</button>
+    </li> `
     })
 }
-console.log(makeMarkup());
-const markup = makeMarkup(getProduct);
-// console.log(markup)
-myList.insertAdjacentElement("beforeend", ...markup);
+
+getProdutcts().then(produtcts=> {
+    const markup = makeMarkup(produtcts)
+    console.log(markup);
+    ulCardEl.insertAdjacentHTML("beforeend",  ...markup)
+
+})
+
+/* 
+3. Додайте обробники подій до кожного товару, щоб при кліку на нього 
+відкривалася сторінка з детальною інформацією про товар. 
+На цій сторінці ви можете відобразити всю додаткову інформацію 
+про товар, таку як опис, ціна, зображення тощо.
+4. Для реалізації функціональності додавання товарів до кошика 
+використовуйте JavaScript. Додайте кнопку "Додати до кошика" 
+на сторінці детальної інформації про товар. При кліку на цю 
+кнопку ви можете зберігати вибраний товар у змінній або масиві, 
+які представляють кошик. Відображайте вибрані товари кошика разом з 
+їхніми назвами та цінами на сторінці кошика. 
+5. Додайте форму оформлення замовлення на сторінку кошика. 
+Ця форма повинна містити поля для введення інформації про 
+замовлення, такої як ім'я, електронна адреса, адреса доставки тощо. 
+Додайте кнопку "Оформити замовлення", яка буде відправляти дані з форми на сервер для обробки.
+*/
